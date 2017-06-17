eae99847d293b99eaa6d142d834f959081cb43cd
Zoltan Altfatter <altfatterz@gmail.com>
2017-06-17 18:13:00 +0200

    working version

--- src/main/java/com/example/Consumer.java
+++ src/main/java/com/example/Consumer.java
@@ -6,12 +6,12 @@ import org.springframework.stereotype.Component;
 /**
  * @author Zoltan Altfatter
  */
-@Component
-public class Consumer {
-
-    @KafkaListener(topics = "someTopic")
-    public void handle(String message) {
-        System.out.println(message);
-    }
-
-}
+//@Component
+//public class Consumer {
+//
+//    @KafkaListener(topics = "someTopic")
+//    public void handle(String message) {
+//        System.out.println(message);
+//    }
+//
+//}
