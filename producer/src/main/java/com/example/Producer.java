38200997ea54592a6c4ca492ff387b5bf60b8a4c
Zoltan Altfatter <altfatterz@gmail.com>
2018-08-14 23:05:31 +0200

    more cleanup

--- producer/src/main/java/com/example/Producer.java
+++ producer/src/main/java/com/example/Producer.java
@@ -24,7 +24,6 @@ public class Producer {
 
     public Producer(KafkaTemplate kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
-        kafkaTemplate.setMessageConverter(new StringJsonMessageConverter());
     }
 
     @Scheduled(fixedDelay = 1000)
